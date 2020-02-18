3620c0fbed2e9768ab3b8cd72ef5d23f0ff1e6bf
elastic <elastic.code@gmail.com>
2020-02-18 14:05:06 +0100

    update example.go

--- example/example.go
+++ example/example.go
@@ -4,12 +4,20 @@ import (
     "github.com/berlincode/factsigner-go/factsigner"
     "fmt"
     "encoding/hex"
+    "net/url"
+    "strconv"
+    "encoding/json"
+    "time"
 )
+
+const baseUrl = "https://berlincode.github.io/digioptions-contracts-web-examples/market_create.html#"
+// const baseUrl = "http://localhost:10002/digioptions-contracts-web-examples/market_create.html#"
+// const baseUrl = "http://localhost:8000/market_create.html#"
 
 func main() {
 
-    //expirationDatetime := uint64(time.Now().Unix() + 2*3600)
-    expirationDatetime := uint64(1580930067+10*3600) // TODO
+    expirationDatetime := uint64(time.Now().Unix() + 2*3600)
+    //expirationDatetime := uint64(1580930067+10*3600) // TODO
     privateKey, err := factsigner.NewPrivateKeyByHex("53b63c349b51ce23793f7ace0d116b7cdb5ee83b8667781feae7a98e5d1043ec")
 
     if err != nil {
@@ -22,33 +30,56 @@ func main() {
         ObjectionPeriod: 3600,
         Config: 0x05, // TODO
         MarketCategory: 0x00, // TODO enum
-        BaseUnitExp: 0x12, // TODO baseUnitExp integer calculcation
+        BaseUnitExp: 18, // TODO baseUnitExp integer calculcation
         Ndigit: 0x02};
 
     factHash := factsigner.FactHash(facts)
-    fmt.Println("factHash", hex.EncodeToString(factHash[:]))
+    fmt.Println("factHash:", hex.EncodeToString(factHash[:]))
+    fmt.Println("")
 
     // create
-    // https://berlincode.github.io/digioptions-contracts-web-examples/market_create.html#?
-    //     settlementDatetime=1572901200&
-    //     baseUnitExp=18&
-    //     objectionPeriod=3600&
-    //     ndigit=0&
-    //     underlyingString=US2605661048&
-    //     signatureFactHash=%7B%0A++%22r%22%3A+%220x7caf800c43d461bffa6d22bfa2e86fcbbeacc3f02cec008ca05640d78116f9de%22%2C%0A++%22s%22%3A+%220x2e6a7600d8bb52e3868f3a6df4ee63e957df89e7e170f6f63b354d91ef7ecb3d%22%2C%0A++%22v%22%3A+28%0A%7D&
-    //     signerAddr=0x6b608020f7a66c727154ed65208e40f9e105f6b8
 
     signatureCreate := factsigner.Sign(factHash, privateKey)
 
-    fmt.Println("Signature market create")
-    fmt.Println("R", hex.EncodeToString(signatureCreate.R[:]))
-    fmt.Println("S", hex.EncodeToString(signatureCreate.S[:]))
-    fmt.Println("V", signatureCreate.V)
+    fmt.Println("Signature market create:")
+    fmt.Println("R:", hex.EncodeToString(signatureCreate.R[:]))
+    fmt.Println("S:", hex.EncodeToString(signatureCreate.S[:]))
+    fmt.Println("V:", signatureCreate.V)
 
-    // https://berlincode.github.io/digioptions-contracts-web-examples/market_create.html#?
-    //     signatureFinal=%7B%0A++%22r%22%3A+%220x91ecdf95a8b63c12fce30d1b431ae17e0a4ff03f4abe2e9c01c57702ba071d52%22%2C%0A++%22s%22%3A+%220x53c1e0816060faf823402cbc810d380d14e929d76b955cda1173245bad4cf5e2%22%2C%0A++%22v%22%3A+28%0A%7D&
-    //     finalValue=0.266&
-    //     signerAddr=0x49b6d897575b0769d45eba7e2de60a16de5b8c13
+    signatureFactHashMap := map[string]string{
+        "r": "0x" + hex.EncodeToString(signatureCreate.R[:]),
+        "s": "0x" + hex.EncodeToString(signatureCreate.S[:]),
+        "v": strconv.FormatUint(uint64(signatureCreate.V), 10)}
+
+
+    var signatureCreateJsonBytes []byte
+    signatureCreateJsonBytes, err2 := json.MarshalIndent(signatureFactHashMap, "", "  ")
+    if err2 != nil {
+        panic(err2)
+    }
+
+    q0 := url.Values{}
+    q0.Add("settlementDatetime", strconv.FormatUint(uint64(facts.ExpirationDatetime), 10)) // TODO settlementDatetime vs ExpirationDatetime
+    q0.Add("baseUnitExp", strconv.FormatUint(uint64(facts.BaseUnitExp), 10))
+    q0.Add("objectionPeriod", strconv.FormatUint(uint64(facts.ObjectionPeriod), 10))
+    q0.Add("config", strconv.FormatUint(uint64(facts.Config), 10))
+    q0.Add("marketCategory", strconv.FormatUint(uint64(facts.MarketCategory), 10))
+    q0.Add("ndigit", strconv.FormatUint(uint64(facts.Ndigit), 10))
+    q0.Add("underlyingString", facts.UnderlyingString)
+    q0.Add("signatureFactHash", string(signatureCreateJsonBytes))
+    q0.Add("signerAddr", "0x" + hex.EncodeToString(factsigner.PubkeyToAddress(privateKey.PublicKey)))
+
+    q0.Add("namedRanges", "[]") // TODO / dummy
+
+    //q0.Add("ethProvider", "ws://localhost:12345") // TODO
+
+    fmt.Println(baseUrl + "?" + q0.Encode())
+
+
+    // settle
+
+    fmt.Println("");
+    fmt.Println("Signature settlement:");
 
     var value [32]byte // TODO
 
@@ -59,13 +90,32 @@ func main() {
 
     factsigner.SettlementHash(settlementData)
 
+    // TODO signatureFinal vs signatureSettle
     signatureSettle := factsigner.Sign(factHash, privateKey)
 
-    fmt.Println("Signature market settle")
-    fmt.Println("R", hex.EncodeToString(signatureSettle.R[:]))
-    fmt.Println("S", hex.EncodeToString(signatureSettle.S[:]))
-    fmt.Println("V", signatureSettle.V)
+    fmt.Println("R:", hex.EncodeToString(signatureSettle.R[:]))
+    fmt.Println("S:", hex.EncodeToString(signatureSettle.S[:]))
+    fmt.Println("V:", signatureSettle.V)
+
+    signatureSettleHashMap := map[string]string{
+        "r": "0x" + hex.EncodeToString(signatureSettle.R[:]),
+        "s": "0x" + hex.EncodeToString(signatureSettle.S[:]),
+        "v": strconv.FormatUint(uint64(signatureSettle.V), 10)}
 
 
+    var signatureSettleJsonBytes []byte
+    signatureSettleJsonBytes, err3 := json.MarshalIndent(signatureSettleHashMap, "", "  ")
+    if err3 != nil {
+        panic(err3)
+    }
+
+    q1 := url.Values{}
+    q1.Add("signatureFinal", string(signatureSettleJsonBytes))
+    q1.Add("finalValue", "1.0")
+    q1.Add("signerAddr", "0x" + hex.EncodeToString(factsigner.PubkeyToAddress(privateKey.PublicKey)))
+
+    //q1.Add("ethProvider", "ws://localhost:12345") // TODO
+
+    fmt.Println(baseUrl + "?" + q1.Encode())
 
 }
